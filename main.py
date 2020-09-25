f164ecb7cf71539b41365d615f87033a2649323c
nmarzz <ndmarshall97@gmail.com>
2020-09-25 19:24:35 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import torch
+import argparse
+
+
+
+
+
+parser = argparse.ArgumentParser(description='Constrastive Learning Experiment')
+parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+                    help='input batch size for training (default: 128)')
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                    help='number of epochs to train (default: 10)')
+parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
+                    help='learning rate (default: 0.1)')
+parser.add_argument('--dropout', type=float, default=0.25, metavar='P',
+                    help='dropout probability (default: 0.25)')
+parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                    help='heavy ball momentum in gradient descent (default: 0.9)')
+parser.add_argument('--data-dir', type=str, default='./data',metavar='DIR')
+args = parser.parse_args()
+args.cuda =  torch.cuda.is_available()
